bc7710f2182819a1b6e862a916e20f1a1c674fb6
Kalman Hazins <kalmanh@gmail.com>
2019-11-04 05:47:04 -0500

    Final app

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,25 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require("jquery")
+// Make jQuery available even in the "views" folder
+global.$ = require("jquery")
+
+require("jquery-ui")
 
 $(function(){
 	// Plain jQuery
 	$('#notice').fadeOut(5000);
+
+	const availableCities = [
+		"Baltimore", "New York", "Orlando", "Washington D.C.", "Philadelphia",
+		"Las Vegas", "Los Angeles", "Denver", "Austin"
+	].sort();
+
+	$('body').on('focus', '#city', function(){
+		$(this).autocomplete( { source: availableCities } );
+	});
+
+	$('body').on('focus', '#arrivalDate', function(){
+		$(this).datepicker( { dateFormat: 'yy-mm-dd' } );
+	})
 });
